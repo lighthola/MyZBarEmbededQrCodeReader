b692115bf7d2418bd41e85c467b3a4f5ecd18b3f
Bevis <bevis_chen@soft-world.com.tw>
2016-06-28 15:43:13 +0800

    Initial Commit

---
+++ TestZBarSDKEmbedReader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestZBarSDKEmbedReader
+//
+//  Created by Bevis Chen on 6/28/16.
+//  Copyright © 2016 Bevis Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
